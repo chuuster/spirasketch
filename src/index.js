175a5eb1281c3279c4f491aa7b3595a8b508906d
Christina <chu.christina@yahoo.com>
2019-01-13 12:42:13 -0800

    Add dynamic nubmer of rotors

--- src/index.js
+++ src/index.js
@@ -1,34 +1,84 @@
 import { Spirograph } from "./point";
 
-export const windowDimensions = { "height": window.innerHeight, "width": window.innerWidth };
-export const canvasEl = document.getElementById("canvas");
-export const ctx = canvasEl.getContext("2d");
+const windowDimensions = { "height": window.innerHeight, "width": window.innerWidth };
+const canvasEl = document.getElementById("canvas");
+const ctx = canvasEl.getContext("2d");
 
 export function clearCanvas() {
   ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
 }
 
+const numRotorsEl = document.getElementById("num-rotors");
+
+// Set Up Canvas 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("webpack is working");
   canvasEl.setAttribute("width", windowDimensions.width * window.devicePixelRatio);
   canvasEl.setAttribute("height", windowDimensions.height * window.devicePixelRatio);
 });
 
+
+// Randomize spirograph on click 
 canvasEl.addEventListener("click", () => {
+  let numRadii = numRotorsEl.value;
+  let radii = [];
+
+  for(let i = 0; i < numRadii; i++) {
+    radii.push(Math.floor(Math.random() * canvasEl.height / (2*numRadii + 1)) + 20);
+  }
+
   let spiro = new Spirograph({
     ctx: ctx,
     cx: (canvasEl.width / 2),
     cy: (canvasEl.height / 2),
-    radius1: Math.floor(Math.random() * canvasEl.height/9) + 25,
-    radius2: Math.floor(Math.random() * canvasEl.height/9) + 25,
-    radius3: Math.floor(Math.random() * canvasEl.height/9) + 25,
-    radius4: Math.floor(Math.random() * canvasEl.height/9) + 25,
-    ratio1: Math.random() * 1000000000,
-    ratio2: Math.random() * 1000000000,
+    radii: radii,
+    ratio1: Math.floor(Math.random() * 10000000000),
+    ratio2: Math.floor(Math.random() * 10000000000),
     thetaLimit: 2 * Math.PI
   });
 
   console.log(spiro);
   clearCanvas();
   spiro.drawSpirograph();
+  document.getElementById("num-rotors").value = "4";
+  numRotorsEl.dispatchEvent(new Event("input"));
+
+
+  for(let i = 1; i <= numRadii; i++) {
+    document.getElementById(`radius${i}`).value = spiro.radii[i-1];
+  }
+
+  document.getElementById("ratio1").value = spiro.ratio1;
+  document.getElementById("ratio2").value = spiro.ratio2;
+});
+
+
+// Options Display Toggle 
+
+const optionsButton = document.getElementById("options-button");
+const optionsPane = document.getElementById("options-pane");
+
+optionsButton.addEventListener("click", () => {
+  optionsPane.classList.toggle("show");
+});
+
+// Options Display Radii 
+
+numRotorsEl.addEventListener("input", () => {
+  let radiiSection = document.getElementById("radii-section");
+  let numRotors = parseInt(numRotorsEl.value);
+
+  while (radiiSection.hasChildNodes()) {
+    radiiSection.removeChild(radiiSection.lastChild);
+  }
+
+  for(let i = 1; i <= numRotors; i++) {
+    let radiusLabel = document.createElement('label');
+    let labelText = document.createTextNode(`radius${i}`);
+    let radiusInput = document.createElement('input');
+    radiusInput.type = "number";
+    radiusInput.id = `radius${i}`;
+    radiusLabel.appendChild(labelText);
+    radiusLabel.appendChild(radiusInput);
+    radiiSection.appendChild(radiusLabel);
+  }
 });
--- src/point.js
+++ src/point.js
@@ -1,37 +1,48 @@
 import {clearCanvas} from "./index";
 
 export class Spirograph {
-  constructor({ctx, cx, cy, radius1, radius2, radius3, radius4, ratio1, thetaLimit}) {
+  constructor({ctx, cx, cy, radii, ratio1, ratio2, thetaLimit}) {
     this.ctx = ctx;
     this.cx = cx;
     this.cy = cy;
-    this.radius1 = radius1;
-    this.radius2 = radius2;
-    this.radius3 = radius3;
-    this.radius4 = radius4;
+    this.radii = radii;
     this.ratio1 = ratio1;
+    this.ratio2 = ratio2;
     this.thetaLimit = thetaLimit;
   }
   
+  sumRadii() {
+    let sum = 0;
+    for (let i = 0; i < this.radii.length; i++) {
+      sum += this.radii[i];
+    } 
+    return sum; 
+  }
+
+  xyOffset(theta) {
+    let xOffset = this.radii[0] + Math.cos(theta * this.ratio1);
+    let yOffset = this.radii[0] + Math.sin(theta * this.ratio1);
+
+    for(let i = 1; i < this.radii.length; i++) {
+      xOffset += this.radii[i] * (Math.cos(theta * this.ratio2));
+      yOffset += this.radii[i] * (Math.sin(theta * this.ratio2));
+    }
+
+    return [xOffset, yOffset];
+  }
+
   drawSpirograph() {
     let x, y, theta;
 
     // Move to starting point (theta = 0)
     this.ctx.beginPath();
-    this.ctx.moveTo(this.cx + this.radius1 + this.radius2 + this.radius3, this.cy);
+    this.ctx.moveTo(this.cx + this.sumRadii(), this.cy);
 
     // Draw segments from theta = 0 to theta = 2PI
     for (theta = 0; theta <= 2*Math.PI; theta += 0.01) {
-      x = this.cx + 
-        (this.radius1 * Math.cos(theta)) + 
-        (this.radius2 * Math.cos(theta * this.ratio1)) + 
-        (this.radius3 * Math.cos(theta * this.ratio1)) + 
-        (this.radius4 * Math.cos(theta * this.ratio1));
-      y = this.cy + 
-        (this.radius1 * Math.sin(theta)) + 
-        (this.radius2 * Math.sin(theta * this.ratio1)) + 
-        (this.radius3 * Math.sin(theta * this.ratio1)) + 
-        (this.radius4 * Math.sin(theta * this.ratio1));
+      let offset = this.xyOffset(theta);
+      x = this.cx + offset[0];
+      y = this.cy + offset[1];
       this.ctx.lineTo(x, y);
       this.ctx.strokeStyle = "rgba(0,0,0,0.5)";
     }
