be13f1c7b09e22df644935907bc80cc7c8454a29
Christina <chu.christina@yahoo.com>
2019-01-12 18:44:14 -0800

    Add options pane and adjust rotors

--- src/index.js
+++ src/index.js
@@ -3,31 +3,32 @@ import { Spirograph } from "./point";
 export const windowDimensions = { "height": window.innerHeight, "width": window.innerWidth };
 export const canvasEl = document.getElementById("canvas");
 export const ctx = canvasEl.getContext("2d");
+
 export function clearCanvas() {
   ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("webpack is working");
-  canvasEl.setAttribute("width", windowDimensions.width);
-  canvasEl.setAttribute("height", windowDimensions.height);
+  canvasEl.setAttribute("width", windowDimensions.width * window.devicePixelRatio);
+  canvasEl.setAttribute("height", windowDimensions.height * window.devicePixelRatio);
 });
 
-canvasEl.addEventListener("keydown", () => {
+canvasEl.addEventListener("click", () => {
   let spiro = new Spirograph({
     ctx: ctx,
-    cx: canvasEl.width / 2,
-    cy: canvasEl.height / 2,
-    radius1: Math.random() * 50,
-    radius2: Math.random() * 100,
-    radius3: Math.random() * 150,
-    ratio1: Math.random() * 1000,
-    ratio2: Math.random() * 1000,
+    cx: (canvasEl.width / 2),
+    cy: (canvasEl.height / 2),
+    radius1: Math.floor(Math.random() * canvasEl.height/9) + 25,
+    radius2: Math.floor(Math.random() * canvasEl.height/9) + 25,
+    radius3: Math.floor(Math.random() * canvasEl.height/9) + 25,
+    radius4: Math.floor(Math.random() * canvasEl.height/9) + 25,
+    ratio1: Math.random() * 1000000000,
+    ratio2: Math.random() * 1000000000,
     thetaLimit: 2 * Math.PI
   });
 
   console.log(spiro);
-  
   clearCanvas();
   spiro.drawSpirograph();
 });
--- src/point.js
+++ src/point.js
@@ -1,15 +1,15 @@
 import {clearCanvas} from "./index";
 
 export class Spirograph {
-  constructor({ctx, cx, cy, radius1, radius2, radius3, ratio1, ratio2, thetaLimit}) {
+  constructor({ctx, cx, cy, radius1, radius2, radius3, radius4, ratio1, thetaLimit}) {
     this.ctx = ctx;
     this.cx = cx;
     this.cy = cy;
     this.radius1 = radius1;
     this.radius2 = radius2;
     this.radius3 = radius3;
+    this.radius4 = radius4;
     this.ratio1 = ratio1;
-    this.ratio2 = ratio2;
     this.thetaLimit = thetaLimit;
   }
   
@@ -21,10 +21,19 @@ export class Spirograph {
     this.ctx.moveTo(this.cx + this.radius1 + this.radius2 + this.radius3, this.cy);
 
     // Draw segments from theta = 0 to theta = 2PI
-    for (theta = 0; theta <= 4*Math.PI; theta += 0.01) {
-      x = this.cx + (this.radius1 * Math.cos(theta)) + (this.radius2 * Math.cos(theta * this.ratio1)) + (this.radius3 * Math.cos(theta * this.ratio2));
-      y = this.cy + (this.radius1 * Math.sin(theta)) + (this.radius2 * Math.sin(theta * this.ratio1)) + (this.radius3 * Math.sin(theta * this.ratio2));
+    for (theta = 0; theta <= 2*Math.PI; theta += 0.01) {
+      x = this.cx + 
+        (this.radius1 * Math.cos(theta)) + 
+        (this.radius2 * Math.cos(theta * this.ratio1)) + 
+        (this.radius3 * Math.cos(theta * this.ratio1)) + 
+        (this.radius4 * Math.cos(theta * this.ratio1));
+      y = this.cy + 
+        (this.radius1 * Math.sin(theta)) + 
+        (this.radius2 * Math.sin(theta * this.ratio1)) + 
+        (this.radius3 * Math.sin(theta * this.ratio1)) + 
+        (this.radius4 * Math.sin(theta * this.ratio1));
       this.ctx.lineTo(x, y);
+      this.ctx.strokeStyle = "rgba(0,0,0,0.5)";
     }
 
     // Apply stroke
